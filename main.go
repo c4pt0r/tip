1521a5ac2db5754b3d328df2890abd73ada4b224
dongxu <i@huangdx.net>
2024-10-08 14:26:14 -0700

    little refactor

--- main.go
+++ main.go
@@ -4,11 +4,9 @@ import (
 	"bufio"
 	"crypto/tls"
 	"database/sql"
-	"encoding/csv"
 	"encoding/json"
 	"flag"
 	"fmt"
-	"io"
 	"log"
 	"os"
 	"path/filepath"
@@ -49,120 +47,6 @@ func parseOutputFormat(format string) OutputFormat {
 	default:
 		return Plain
 	}
-}
-
-type ResultIOWriter interface {
-	Write(rows []RowResult) error
-	Flush() error
-}
-
-type CSVResultIOWriter struct {
-	writer *csv.Writer
-}
-
-func NewCSVResultIOWriter(writer io.Writer) *CSVResultIOWriter {
-	return &CSVResultIOWriter{
-		writer: csv.NewWriter(writer),
-	}
-}
-
-func (w *CSVResultIOWriter) Write(rows []RowResult) error {
-	for _, row := range rows {
-		record := make([]string, len(row.colValues))
-		for i, val := range row.colValues {
-			record[i] = formatCSVValue(val)
-		}
-		if err := w.writer.Write(record); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func (w *CSVResultIOWriter) Flush() error {
-	w.writer.Flush()
-	return w.writer.Error()
-}
-
-type PlainResultIOWriter struct {
-	writer *bufio.Writer
-}
-
-func NewPlainResultIOWriter(writer io.Writer) *PlainResultIOWriter {
-	return &PlainResultIOWriter{
-		writer: bufio.NewWriter(writer),
-	}
-}
-
-func (w *PlainResultIOWriter) Write(rows []RowResult) error {
-	for _, row := range rows {
-		for i, col := range row.colNames {
-			val := row.colValues[i]
-			_, err := fmt.Fprintf(w.writer, "%s: %s ", col, formatValue(val))
-			if err != nil {
-				return err
-			}
-		}
-		_, err := w.writer.WriteString("\n")
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func (w *PlainResultIOWriter) Flush() error {
-	return w.writer.Flush()
-}
-
-type JSONResultIOWriter struct {
-	writer *bufio.Writer
-	first  bool
-}
-
-func NewJSONResultIOWriter(writer io.Writer) *JSONResultIOWriter {
-	return &JSONResultIOWriter{
-		writer: bufio.NewWriter(writer),
-		first:  true,
-	}
-}
-
-func (w *JSONResultIOWriter) Write(rows []RowResult) error {
-	for _, row := range rows {
-		if w.first {
-			_, err := w.writer.WriteString("[")
-			if err != nil {
-				return err
-			}
-			w.first = false
-		} else {
-			_, err := w.writer.WriteString(",")
-			if err != nil {
-				return err
-			}
-		}
-
-		jsonData, err := json.Marshal(row)
-		if err != nil {
-			return err
-		}
-
-		_, err = w.writer.Write(jsonData)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func (w *JSONResultIOWriter) Flush() error {
-	if !w.first {
-		_, err := w.writer.WriteString("]")
-		if err != nil {
-			return err
-		}
-	}
-	return w.writer.Flush()
 }
 
 // Load configuration from a file
